802001819380a6eb98bc935ae158c75ff472d83d
Vadim Zhamkov <vadim.zhamkov@gmail.com>
2016-12-04 23:42:06 +0300

    Represents style encapsulation bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+
+import { ROUTES } from './app.routes';
+
+import { InputMaskModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InputMaskModule,
+    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
   providers: [],
   bootstrap: [AppComponent]
